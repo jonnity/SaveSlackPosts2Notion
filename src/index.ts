50ad4c70e0fd323989383f4bdd2f0c7a7b6a411d
jonnity <jonny1996_ty@yahoo.co.jp>
2022-08-25 11:28:28 +0900

    express init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const port = 3000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ name: "john doe" });
+});
+app.post("/");
+
+app.listen(port);

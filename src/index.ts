47911de9347a323cf1dd729aa81fbd6cf62f9cd1
DESKTOP-PFMOHCA\jonny <jonny1996_ty@yahoo.co.jp>
2022-08-25 22:13:21 +0900

    slack botのチャレンジに対応

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import express from "express";
 
 const port = 3000;
@@ -6,10 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ name: "john doe" });
+app.post("/", (req, res) => {
+  const token = req.body.token;
+  const challenge = req.body.challenge;
+  const type = req.body.type;
+  if (
+    token === process.env.SLACK_VERIFICATION_TOKEN &&
+    type === "url_verification"
+  ) {
+    res.status(200).send(challenge);
+  } else {
+    res.status(400).end();
+  }
 });
-app.post("/");
 
 app.listen(port);
 
